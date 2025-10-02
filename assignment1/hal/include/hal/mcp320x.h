256c4fad4d5fe1e5318ad9606a52fecf84313e63
Matthew Goodman <matthewpetersg@gmail.com>
2025-10-02 14:11:25 -0700

    Change to init-cleanup architechture for ADC.

---
+++ assignment1/hal/include/hal/mcp320x.h
@@ -0,0 +1,43 @@
+
+#ifndef _MCP320x_H_
+#define _MCP320x_H_
+
+#define MCP320x_PATH "/dev/spidev0.0"
+#define MCP320x_SPI_FREQUENCY 1000000
+#define MCP320x_BITS_PER_WORD 8
+
+typedef enum {
+    MCP320x_SPI_ERROR = -1,
+    MCP320x_OK = 0,
+} MCP3204Result;
+
+typedef enum {
+    // MCP3204 and MCP3208
+    MCP320x_CH0 = 0x000,
+    MCP320x_CH1 = 0x001,
+    MCP320x_CH2 = 0x010,
+    MCP320x_CH3 = 0x011,
+    // MCP3208
+    MCP320x_CH4 = 0x100,
+    MCP320x_CH5 = 0x101,
+    MCP320x_CH6 = 0x110,
+    MCP320x_CH7 = 0x111,
+} MCP3204Channel;
+
+/// @brief Create a reference to the peripheral.
+/// @return 
+MCP3204Result mcp3204_init(int* fd_out);
+
+/// @brief Get the 12 bit value for a specific channel.
+/// @param fd the reference to the peripheral.
+/// @param channel The channel index (0 upto 7 for MCP3208).
+/// @param value_out The place to put the value.
+/// @return result.
+MCP3204Result mcp320x_get(int fd, MCP3204Channel channel, int* value_out);
+
+/// @brief 
+/// @param fd 
+/// @return 
+MCP3204Result mcp3204_cleanup(int fd);
+
+#endif
---
+++ assignment1/hal/src/mcp320x.c
@@ -0,0 +1,97 @@
+#include "hal/mcp320x.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+MCP3204Result mcp3204_init(int* fd_out) {
+    int fd = open(MCP320x_PATH, O_RDWR);
+    if (fd < 0) {
+        perror("Can't open SPI");
+        return MCP320x_SPI_ERROR;
+    }
+
+    *fd_out = fd;
+
+    return MCP320x_OK;
+}
+
+MCP3204Result mcp3204_cleanup(int fd) {
+    if (close(fd) < 0) {
+        perror("Can't close SPI");
+        return MCP320x_SPI_ERROR;
+    }
+
+    return MCP320x_OK;
+}
+
+MCP3204Result mcp3204_get(int fd, MCP3204Channel channel, uint16_t* value_out) {
+    uint8_t tx_0 = (0x1 << 7 | 0x1 << 6) | channel << 3; // { START[0], SINGLE/DIFF[0], CHANNEL[2, 1, 0] }
+
+    uint8_t tx[] = { tx_0 };
+    uint8_t rx[2] = { 0x0, 0x0 };
+
+    if (fd < 0) {
+        perror("open");
+        return MCP320x_SPI_ERROR;
+    }
+
+    struct spi_ioc_transfer tr = {
+        .tx_buf = *tx,
+        .rx_buf = *rx,
+        .len = sizeof(tx) + sizeof(rx),
+        .speed_hz = MCP320x_SPI_FREQUENCY,
+        .bits_per_word = MCP320x_BITS_PER_WORD,
+        .delay_usecs = 0,
+    };
+
+    int io_res = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+
+    close(fd);
+
+    if (io_res < 1) {
+        perror("Can't send spi message");
+        return MCP320x_SPI_ERROR;
+    }
+
+    *value_out = (rx[0] | (rx[1] << 8)) & 0xFFF;
+
+    return MCP320x_OK;
+}
+
+/*
+fn read(&mut self, mode: Mode, address: u8) -> Result<Reading> {
+        // START[1] + MODE[1] + ADDR[1/3] + SAMPLE[1] + NULL[1] + DATA[10-13]
+        let size = 1 + 1 + self.channels.bit_size() + 1 + 1 + self.resolution.0;
+        let bytes = (f32::from(size) / 8f32).ceil() as u8;
+
+        let command: u32 = 1u32 << u32::from(size - 1)
+            | ((mode as u32) << u32::from(size - 2))
+            | ((u32::from(address)) << (self.resolution.0 + 2)) as u32;
+
+        let mut tx: Vec<u8> = Vec::with_capacity(bytes as usize);
+        for i in (0..bytes).rev() {
+            let shift = u32::from(8u8 * i);
+            tx.push(((command & (0b_1111_1111u32 << shift)) >> shift) as u8);
+        }
+
+        let mut rx: Vec<u8> = Vec::with_capacity(bytes as usize);
+        for _ in 0..bytes {
+            rx.push(0);
+        }
+
+        self.spi.transfer(&mut rx.as_mut_slice(), &tx.as_slice())?;
+
+        let mut result: u32 = 0;
+        for (i, byte) in rx.iter().enumerate() {
+            result |= (u32::from(*byte) << (u32::from(bytes - 1 - i as u8) * 8)) as u32;
+        }
+
+        debug_assert_eq!(result >> u32::from(self.resolution.0), 0);
+
+        Ok(Reading::new(result as u16, self.resolution.range().1))
+    }
+*/
